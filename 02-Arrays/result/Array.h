c11d56896fdddd73db82ed07bb6a179db01b9b91
leolock <liusp0229@163.com>
2020-12-21 20:37:31 +0800

    get&set

--- 02-Arrays/result/Array.h
+++ 02-Arrays/result/Array.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <cassert>
+
 class Array {
 public:
     // 构造函数，传入数组的容量capacity构造Array
@@ -29,6 +32,44 @@ public:
         return size == 0;
     }
 
+    void add(int index, int e){
+        assert(size<capacity && index>=0 && index<=size );
+        for(int i=size-1;i>=index;--i){
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    void addFirst(int e){
+        add(0, e);
+    }
+
+    void addLast(int e){
+        add(size, e);
+    }
+
+    int get(int index){
+        assert(index >= 0 && index < size);
+        return data[index];
+    }
+
+    void set(int index, int e){
+        assert(index >= 0 && index < size);
+        data[index] = e;
+    }
+
+    void print(){
+        assert(size > 0);
+        std::cout << "Array: size = "<< size << ",capacity = " << getCapacity() << std::endl;
+        std::cout << "[";
+        for(int i = 0; i<size; ++i){
+            std::cout <<data[i];
+            if(i != size-1) std::cout << ",";
+        }
+        std::cout << "]";
+        std::cout << std::endl;
+    }
 private:
     int *data;
     int size;
